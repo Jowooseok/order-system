e08cf33901c2e3deb141a3bad42de4290b4dcc23
Jowooseok <whdntjrsla12@naver.com>
2020-09-08 11:09:32 +0900

    주소/키워드 버튼 추가 기능x

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Row, Button, Divider, Input } from 'antd';
+import { Menu, Dropdown, Button, Divider, Input } from 'antd';
 import Link from 'next/link';
 
 const { Search } = Input;
+
+const menu = (
+    <Menu>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                1st menu item
+        </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                2nd menu item
+        </a>
+        </Menu.Item>
+    </Menu>
+);
 
 const home = () => {
 
     const [address, setAddress] = useState('청원구청');
     const [radius, setRadius] = useState(1500);
+    const [searchMethod, setSearchMethod] = useState('주소');
+
 
     useEffect(() => {
         let container = document.getElementById('map');
@@ -17,7 +34,7 @@ const home = () => {
         };
 
         let map = new kakao.maps.Map(container, options);
-        
+
 
         // 주소로 좌표를 검색합니다
         let geocoder = new kakao.maps.services.Geocoder();
@@ -41,25 +58,25 @@ const home = () => {
                 // infowindow.open(map, marker);
 
                 // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-             
-                if(!isNaN(radius)){ //한글/영어/특수문자등 입력 방지
-                  
-                const circle = new kakao.maps.Circle({
-                    
-                    center : new kakao.maps.LatLng(coords.Ha, coords.Ga),  // 원의 중심좌표 입니다 
 
-                    radius: radius, // 미터 단위의 원의 반지름입니다 
-                    strokeWeight: 5, // 선의 두께입니다 
-                    strokeColor: '#75B8FA', // 선의 색깔입니다
-                    strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-                    strokeStyle: 'dashed', // 선의 스타일 입니다
-                    fillColor: '#CFE7FF', // 채우기 색깔입니다
-                    fillOpacity: 0.7  // 채우기 불투명도 입니다   
-                }); 
-                
-                // 지도에 원을 표시합 니다 
-                circle.setMap(map);
-            }
+                if (!isNaN(radius)) { //한글/영어/특수문자등 입력 방지
+
+                    const circle = new kakao.maps.Circle({
+
+                        center: new kakao.maps.LatLng(coords.Ha, coords.Ga),  // 원의 중심좌표 입니다 
+
+                        radius: radius, // 미터 단위의 원의 반지름입니다 
+                        strokeWeight: 5, // 선의 두께입니다 
+                        strokeColor: '#75B8FA', // 선의 색깔입니다
+                        strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+                        strokeStyle: 'dashed', // 선의 스타일 입니다
+                        fillColor: '#CFE7FF', // 채우기 색깔입니다
+                        fillOpacity: 0.7  // 채우기 불투명도 입니다   
+                    });
+
+                    // 지도에 원을 표시합 니다 
+                    circle.setMap(map);
+                }
 
                 map.setCenter(coords);
             }
@@ -67,13 +84,23 @@ const home = () => {
 
     }, [address, radius])
 
+    const searchClick = () => {
+        if(searchMethod==='주소'){
+            setSearchMethod('키워드')
+        }else{
+            setSearchMethod('주소')
+        }
+    }
+    
+
     return (
         <>
             <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=b31117910c5af1f02ade4940f5762a07&libraries=services,clusterer,drawing"></script>
 
-            <div id='map' style={{ height: '100vh' }}>
-                <Search style={{ width: '70%', position: 'absolute', zIndex: '2' }} placeholder="주소 입력" onSearch={value => {setAddress(value)}} enterButton />
-                <Input style={{width:'30%', float:'right', zIndex:'2'}} onChange={e=>{setRadius(e.target.value)}}  placeholder='반경미터입력'  />
+            <div id='map' style={{ height: '100vh', zIndex: '0' }}>
+                <Button style={{ width: '10%', float: 'left', zIndex: '2' }} onClick={searchClick} >{searchMethod}</Button>
+                <Search style={{ width: '60%', position: 'absolute', zIndex: '2' }} placeholder="주소 입력" onSearch={value => { setAddress(value) }} enterButton />
+                <Input style={{ width: '20%', float: 'right', zIndex: '2' }} onChange={e => { setRadius(e.target.value) }} placeholder='반경미터입력' />
             </div>
         </>
     )
